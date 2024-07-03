4bcc2c262753e036dfd3e2793f393d61f7cb9da8
santiagoamayalpz <122042569@upq.edu.mx>
2024-07-03 17:28:39 -0600

    Practica15

--- Practica15.sql
+++ Practica15.sql
@@ -1,22 +1,20 @@
 
-CREATE PROCEDURE Suscripcion
-    @UsuarioID INT,
-    @FechaInicio DATE,
-    @FechaFin DATE,
-    @Tipo NVARCHAR(50)
+CREATE PROCEDURE NuevasSuscripciones
+@Id_usuario INT,
+@Tipo NVARCHAR(50),
+@Fecha_inicio DATE,
+@Fecha_fin DATE
 AS
 BEGIN
-    BEGIN TRANSACTION;
-
-    INSERT INTO Suscripciones (UsuarioID, FechaInicio, FechaFin, Tipo)
-    VALUES (@UsuarioID, @FechaInicio, @FechaFin, @Tipo);
-
-    UPDATE Usuarios
-    SET FechaRegistro = @FechaInicio
-    WHERE UsuarioID = @UsuarioID;
-
-    COMMIT TRANSACTION;
+BEGIN TRANSACTION
+INSERT INTO Suscripciones(UsuarioID, Tipo, FechaInicio, FechaFin) VALUES (@Id_usuario, @Tipo, @Fecha_inicio, @Fecha_fin)
+UPDATE Usuarios
+SET FechaRegistro = @Fecha_inicio
+WHERE UsuarioID = @Id_usuario;
+COMMIT TRANSACTION;
 END;
+
+EXEC NuevasSuscripciones 6, 'Anual','2023-06-13', '2024-07-13'
 
 select * from Suscripciones
 select * from Usuarios
